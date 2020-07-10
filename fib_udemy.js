1c1b5748fd45ba25b4ad4da04124a7dd4b4f1728
Crew Spence <csspence123@gmail.com>
2020-07-10 05:43:46 -0500

    Created function stub for toy problem.

---
+++ fib_udemy.js
@@ -0,0 +1,15 @@
+/*
+JavaScript Algorithms and Data Structures Masterclass
+
+fib
+Write a recursive function called fib which accepts a number and returns the nth number in the Fibonacci sequence.  Recall that the Fibonacci sequence is the sequence of whole numbers 1, 2, 3, 4, 5, 6, ... which start with 1, and where every nuber thereafter is equal to the sum of the previous two numbers.
+*/
+
+function fib(){
+ 
+}
+
+// fib(4) // 3
+// fib(10) // 55
+// fib(28) // 317811
+// fib(35) // 9227465
