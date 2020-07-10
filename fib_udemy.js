1c5b0b579808997375f1a9f12333aa42969c8ab3
Crew Spence <csspence123@gmail.com>
2020-07-10 06:13:23 -0500

    Wrote working solution.

--- fib_udemy.js
+++ fib_udemy.js
@@ -5,8 +5,29 @@ fib
 Write a recursive function called fib which accepts a number and returns the nth number in the Fibonacci sequence.  Recall that the Fibonacci sequence is the sequence of whole numbers 1, 2, 3, 4, 5, 6, ... which start with 1, and where every nuber thereafter is equal to the sum of the previous two numbers.
 */
 
-function fib(){
- 
+const fib = (num) => {
+  let counter = 3;
+  let num1 = 1;
+  let num2 = 1;
+  let current = 2;
+  const innerFunc = (number) => {
+    num1 = num2;
+    num2 = current;
+    current = num1 + num2;
+    counter++;
+    if(num === counter) {
+      return current;
+    } else {
+      return innerFunc(num);
+    }
+  }
+  if(num === 1 || num === 2) {
+    return 1;
+  } else if (num === counter) {
+    return current;
+  } else {
+    return innerFunc(num);
+  }
 }
 
 // fib(4) // 3
